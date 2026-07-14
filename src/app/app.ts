e0cb70643c158c11a8301039a6c35f24373df1bf
Patrik Behul <behulpatrik@gmail.com>
2026-07-14 13:07:14 +0200

    Added Basic Components For The Homepage.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from "@angular/core"
+import { RouterOutlet } from "@angular/router"
+import { NavigationBar } from "./navigation-bar/navigation-bar"
+import { Banner } from "./banner/banner"
+import { SearchBar } from "./search-bar/search-bar"
+import { Catalog } from "./catalog/catalog"
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  selector: "app-root",
+  imports: [RouterOutlet, NavigationBar, Banner, SearchBar, Catalog],
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss"
 })
+
 export class App {
-  protected readonly title = signal('restaurant');
+  
 }
---
+++ src/app/navigation-bar/navigation-bar.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-navigation-bar",
+  imports: [],
+  templateUrl: "./navigation-bar.html",
+  styleUrl: "./navigation-bar.scss",
+})
+
+export class NavigationBar {}
