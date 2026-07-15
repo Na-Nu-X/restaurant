6ad3b4a10b3b86bd7d696e7f43d3f404983a462e
Patrik Behul <behulpatrik@gmail.com>
2026-07-15 22:29:50 +0200

    Added Contact Form UI.

--- restaurant/src/app/app.ts
+++ restaurant/src/app/app.ts
@@ -1,18 +1,13 @@
-import { Component, signal } from "@angular/core"
+import { Component } from "@angular/core"
 import { RouterOutlet } from "@angular/router"
 import { NavigationBar } from "./navigation-bar/navigation-bar"
-import { Banner } from "./banner/banner"
-import { SearchBar } from "./search-bar/search-bar"
-import { Catalog } from "./catalog/catalog"
 import { Footer } from "./footer/footer"
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, NavigationBar, Banner, SearchBar, Catalog, Footer],
+  imports: [RouterOutlet, NavigationBar, Footer],
   templateUrl: "./app.html",
   styleUrl: "./app.scss"
 })
 
-export class App {
-  
-}
+export class App { }
--- restaurant/src/app/home/home.ts
+++ restaurant/src/app/home/home.ts
@@ -1,16 +1,18 @@
-import { Component } from "@angular/core";
-import { Banner } from '../banner/banner'; 
-import { SearchBar } from '../search-bar/search-bar'; 
-import { Catalog } from '../catalog/catalog';
+import { Component } from "@angular/core"
+import { Banner } from "../banner/banner"
+import { SearchBar } from "../search-bar/search-bar"
+import { Catalog } from "../catalog/catalog"
+import { ContactForm } from "../contact-form/contact-form"
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   imports: [
     Banner,
     SearchBar,
-    Catalog
+    Catalog,
+    ContactForm
   ],
-  templateUrl: './home.html',
-  styleUrl: './home.scss',
+  templateUrl: "./home.html",
+  styleUrl: "./home.scss"
 })
 export class Home {}
